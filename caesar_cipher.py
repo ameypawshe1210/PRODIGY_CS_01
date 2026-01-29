73fc27258528b5620a592d6aba1353fc8bd968c9
Amey Pawshe <ameypawshe12@gmail.com>
2026-01-29 15:26:26 +0530

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,4 +1,12 @@
 def encrypt(text, shift):
+    result = ""
+    for char in text:
+        if char.isupper():
+            result += chr((ord(char) - 65 + shift) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) - 97 + shift) % 26 + 97)
+        else:
+            result += char
     return result
 
 
